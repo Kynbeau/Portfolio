8832b282b565e8c3a3df6cf90e08cb058a350c0c
Kynbeau <Kent.andrey.aguhob@gmail.com>
2025-12-11 05:15:23 +0800

    4th try

--- docker/start.sh
+++ docker/start.sh
@@ -6,6 +6,10 @@ envsubst '${PORT}' < /etc/nginx/templates/default.conf.template > /etc/nginx/con
 
 chown -R www-data:www-data /var/www/storage /var/www/bootstrap/cache
 
+if [ -z "${APP_KEY}" ]; then
+  export APP_KEY=$(php -r "echo 'base64:'.base64_encode(random_bytes(32));")
+fi
+
 php /var/www/artisan config:clear || true
 php /var/www/artisan route:clear || true
 php /var/www/artisan view:clear || true
